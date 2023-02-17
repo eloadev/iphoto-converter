2503e7070e3068533dd4143ca6b6724d48d72329
Eloa Mello <eloamello126@gmail.com>
2023-02-17 20:49:28 -0300

    refactor: implemented converter class

--- src/main.py
+++ src/main.py
@@ -3,22 +3,39 @@ import shutil
 from PIL import Image
 from pillow_heif import register_heif_opener
 
-input_dir = "images/input_image_heic"
-output_dir = "images/output_image_png"
-dest_dir = "images/already_converted_images"
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+class ImageConverter:
+    def __init__(self, input_dir, output_dir, dest_dir):
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+        self.dest_dir = dest_dir
 
-if not os.path.exists(dest_dir):
-    os.makedirs(dest_dir)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-for filename in os.listdir(input_dir):
-    if filename.endswith(".HEIC") or filename.endswith(".heic"):
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
 
+    def convert_images(self):
         register_heif_opener()
-        with Image.open(os.path.join(input_dir, filename)) as img:
-            output_filename = os.path.splitext(filename)[0] + ".png"
-            output_path = os.path.join(output_dir, output_filename)
-            img.save(output_path, "PNG")
-            shutil.move(os.path.join(input_dir, filename), os.path.join(dest_dir, filename))
+
+        for filename in os.listdir(self.input_dir):
+            if filename.lower().endswith(".heic"):
+                input_path = os.path.join(self.input_dir, filename)
+
+                with Image.open(input_path) as img:
+                    output_filename = os.path.splitext(filename)[0] + ".png"
+                    output_path = os.path.join(self.output_dir, output_filename)
+                    img.save(output_path, "PNG")
+
+                shutil.move(input_path, os.path.join(self.dest_dir, filename))
+
+
+if __name__ == "__main__":
+    input_dir = "images/input_image_heic"
+    output_dir = "images/output_image_png"
+    dest_dir = "images/already_converted_images"
+
+    converter = ImageConverter(input_dir, output_dir, dest_dir)
+    converter.convert_images()
+
